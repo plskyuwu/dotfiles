a0d882147ccce76a213b30884ac183fc542c9df9
plskyuwu <gottsteinmatej@gmail.com>
2024-09-25 19:50:45 +0200

    Added nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,21 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.autoindent = true
+vim.opt.cc = "80"
+vim.opt.cursorline = true
+vim.opt.showmatch = true
+
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.termguicolors = true
+vim.cmd("colorscheme vim")
