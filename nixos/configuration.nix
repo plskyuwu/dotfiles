b94815dfe8a46181c65079c2b7e945c47bffccf2
plskyuwu <gottsteinmatej@gmail.com>
2024-10-27 19:02:38 +0100

    Added sway and nixos config

---
+++ nixos/configuration.nix
@@ -0,0 +1,160 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Prague";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "cs_CZ.UTF-8";
+    LC_IDENTIFICATION = "cs_CZ.UTF-8";
+    LC_MEASUREMENT = "cs_CZ.UTF-8";
+    LC_MONETARY = "cs_CZ.UTF-8";
+    LC_NAME = "cs_CZ.UTF-8";
+    LC_NUMERIC = "cs_CZ.UTF-8";
+    LC_PAPER = "cs_CZ.UTF-8";
+    LC_TELEPHONE = "cs_CZ.UTF-8";
+    LC_TIME = "cs_CZ.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.plskyuwu = {
+    isNormalUser = true;
+    description = "plskyuwu";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # CLI
+    neovim
+    vim
+    lunarvim
+    git
+    fastfetch
+    mc
+    gh
+    wget
+    fish
+    btop
+    htop
+    unzip
+    unrar
+
+    # DE/WM
+    grim
+    slurp
+    sway-contrib.grimshot
+    mako
+    wl-clipboard
+    waybar
+    kitty
+    wofi
+    pavucontrol
+    clipman
+    pcmanfm
+    imv
+
+    # GUI
+    firefox
+    vesktop
+    spotify
+    steam
+    netbeans
+    prismlauncher
+    gimp
+    inkscape
+    libreoffice-qt6-fresh
+
+    # Other
+    temurin-bin-21
+    openjdk17-bootstrap
+    openjdk8-bootstrap
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  security.rtkit.enable = true;
+
+  # List services that you want to enable:
+  services.gnome.gnome-keyring.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  networking.firewall.allowedTCPPorts = [ 57621 ];
+  networking.firewall.allowedUDPPorts = [ 5353 ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
