d2d82d2dbdfff461f417a52229924362606a8910
plskyuwu <gottsteinmatej@gmail.com>
2025-06-01 13:26:28 +0200

    Updated locale.conf and simple neovim config

--- .config/nvim/simple-init.lua
+++ .config/nvim/simple-init.lua
@@ -4,13 +4,13 @@ vim.opt.ignorecase = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.cindent = true
-vim.opt.colorcolumn = {"80", "120"}
+vim.opt.colorcolumn = {"80", "100", "120"}
 vim.opt.cursorline = true
 
 vim.opt.mouse = "a"
